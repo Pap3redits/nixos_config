5283d4dc67784af05450af231d8cd3fb562e43f7
Christian Schmidt <pap3redits@gmail.com>
2026-01-16 11:05:19 -0500

    tset

--- home/ui/hyprland_test.nix
+++ home/ui/hyprland_test.nix
@@ -5,14 +5,6 @@
     enable = true;
   };
 
-  xdg.configFile =
-    let
-      mkSymlink = config.lib.file.mkOutOfStoreSymlink;
-    in
-    {
-      "hypr".source = mkSymlink "/etc/nixos/home/conf/hypr/";
-    };
-
   programs.hyprlock.enable = true;
 
 }
--- home/ui/waybar.nix
+++ home/ui/waybar.nix
@@ -1,13 +1,14 @@
-{config, pkgs, ...}:{
+{ config, pkgs, ... }:
+{
 
-  xdg.configFile = 
+  xdg.configFile =
     let
       mkSymlink = config.lib.file.mkOutOfStoreSymlink;
     in
     {
       "waybar".source = mkSymlink "/etc/nixos/home/conf/waybar/";
+      "hypr".source = mkSymlink "/etc/nixos/home/conf/hypr/";
     };
-    
 
   programs.waybar = {
     enable = true;
