56878781f76462c1c7ad15331450d7ad8a7028a1
Christian Schmidt <pap3redits@gmail.com>
2026-01-16 09:08:47 -0500

    removing hyprpaper

--- home/ui/hyprland_test.nix
+++ home/ui/hyprland_test.nix
@@ -5,13 +5,13 @@
     enable = true;
   };
   home.file.".config/hypr".source = ../conf/hypr;
-  services.hyprpaper = {
-    enable = true;
-    settings = {
-      preload = "/etc/nixos/assets/wallpapers/polyscape_pillars_2.png";
-      wallpaper = ",/etc/nixos/assets/wallpapers/polyscape_pillars_2.png";
-    };
-  };
+  #services.hyprpaper = {
+  #  enable = true;
+  #  settings = {
+  #    preload = "/etc/nixos/assets/wallpapers/polyscape_pillars_2.png";
+  #    wallpaper = ",/etc/nixos/assets/wallpapers/polyscape_pillars_2.png";
+  #  };
+  #};
 
   programs.hyprlock.enable = true;
 
