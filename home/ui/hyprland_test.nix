7caa5fccd7b54f926141255df90de06ea0293e07
Christian Schmidt <pap3redits@gmail.com>
2026-01-16 09:47:01 -0500

    copied hyprland over

--- home/ui/hyprland_test.nix
+++ home/ui/hyprland_test.nix
@@ -4,15 +4,15 @@
   wayland.windowManager.hyprland = {
     enable = true;
   };
-  services.hyprpaper.enable = true;
+
   home.file.".config/hypr".source = ../conf/hypr;
-  #services.hyprpaper = {
-  #  enable = true;
-  #  settings = {
-  #    preload = "/etc/nixos/assets/wallpapers/polyscape_pillars_2.png";
-  #    wallpaper = ",/etc/nixos/assets/wallpapers/polyscape_pillars_2.png";
-  #  };
-  #};
+  services.hyprpaper = {
+    enable = true;
+    #settings = {
+    #  preload = "/etc/nixos/assets/wallpapers/polyscape_pillars_2.png";
+    #  wallpaper = ",/etc/nixos/assets/wallpapers/polyscape_pillars_2.png";
+    #};
+  };
 
   programs.hyprlock.enable = true;
 
