caf1aa0d3b5c52f244afa41d9f354187b8762fe6
Christian <pap3redits@gmail.com>
2026-01-22 14:55:51 -0500

    Update wofi.nix

--- home/ui/wofi.nix
+++ home/ui/wofi.nix
@@ -1,33 +1,33 @@
-{config, pkgs, ...}:{
-
+{ config, pkgs, ... }:
+{
 
   programs.wofi = {
-      enable = true;
-      style = ''
+    enable = true;
+    style = ''
         window {
-  margin: 0px;
-  border: 1px solid #d3c6aa;
-  border-radius: 10px;
-  background-color: #2d353b;
-}
+        margin: 0px;
+        border: 1px solid #d3c6aa;
+        border-radius: 10px;
+        background-color: #2d353b;
+      }
 
-#text {
-  margin: 5px;
-  color: #d3c6aa
-}
+      #text {
+        margin: 5px;
+        color: #d3c6aa
+      }
 
-#entry:selected {
-  color: #dbbc7f;
-  background-color: #2d353b;
-  border: none;
-}
+      #entry:selected {
+        color: #dbbc7f;
+        background-color: #2d353b;
+        border: none;
+      }
 
-#input {
-  margin: 5px;
-  border: none;
-  color: 
-}
-      '';
-    };
+      #input {
+        margin: 5px;
+        border: none;
+        color:
+      }
+    '';
+  };
 
 }
